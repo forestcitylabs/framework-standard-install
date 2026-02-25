ae88e4b46f69ccd7fc02c73c22f0dfff652b1507
Luke Bainbridge <luke.bainbridge@gmail.com>
2026-02-25 15:04:33 -0500

    Use new command loader paradigm.

--- config/packages/console.php
+++ config/packages/console.php
@@ -9,24 +9,46 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
+use ForestCityLabs\Framework\Command\Loader\CommandLoader;
+use ForestCityLabs\Framework\Utility\ClassDiscovery\ChainedDiscovery;
+use ForestCityLabs\Framework\Utility\ClassDiscovery\ManualDiscovery;
+use ForestCityLabs\Framework\Utility\ClassDiscovery\ScanDirectoryDiscovery;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\CommandLoader\CommandLoaderInterface;
 use Symfony\Component\Console\Helper;
 use Symfony\Component\Console\Helper\HelperSet;
 
 use function DI\add;
+use function DI\autowire;
 use function DI\create;
 use function DI\get;
+use function DI\string;
 
 return [
+    // Command loader.
+    CommandLoaderInterface::class => autowire(CommandLoader::class)
+        ->constructorParameter('discovery', get('console.class_discovery')),
+
     // Main application.
     Application::class => create()
         ->constructor('Forest City Labs Framework Console')
         ->method('setHelperSet', get(HelperSet::class))
-        ->method('addCommands', get('console.commands')),
+        ->method('setCommandLoader', get(CommandLoaderInterface::class)),
 
     // Helper set.
     HelperSet::class => create()
         ->constructor(get('console.helpers')),
+
+    // Console class discovery.
+    'console.class_discovery' => create(ChainedDiscovery::class)
+        ->constructor(get('console.discovery')),
+    'console.discovery' => add([
+        create(ManualDiscovery::class)->constructor(get('console.commands')),
+        create(ScanDirectoryDiscovery::class)->constructor(get('console.paths')),
+    ]),
+    'console.paths' => add([
+        string('{app.project_root}/src/Command'),
+    ]),
 
     // Helpers.
     'console.helpers' => add([
